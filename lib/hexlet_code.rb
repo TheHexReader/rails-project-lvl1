817d9007a7e4493ebd799afe24ff8f4a218968e2
Hex <thehexreader@gmail.com>
2022-08-03 12:03:19 +0300

    added method Tag.build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,6 +3,14 @@
 require_relative "hexlet_code/version"
 
 module HexletCode
-  class Error < StandardError; end
-  # Your code goes here...
+  module Tag
+    def Tag.build(tag, **attr)
+      out = "<#{tag}#{attr.nil? ? '' : " #{(attr.map {|key, item| "#{key.to_s}=\"#{item.to_s}\"" }).join(', ')}"}>"
+      if !%w[br img input].include? tag
+        tag_inner_text = yield
+        out = "#{out}#{tag_inner_text}</#{tag}>"
+      end
+      return out
+    end
+  end
 end
