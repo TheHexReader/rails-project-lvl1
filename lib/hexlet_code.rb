98aa41c54e49ca6acb848b960a8d9377bd23fcb0
Hex <thehexreader@gmail.com>
2022-08-15 12:28:38 +0300

    fixing

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
-require_relative 'hexlet_code/tag'
-require_relative 'hexlet_code/form_builder'
 
 # HexletCode module
 module HexletCode
-  autoload(:Tag, './lib/hexlet_code/tag.rb')
-  autoload(:HexletCode, './lib/hexlet_code/form_builder.rb')
+  autoload(:Tag, File.expand_path('./lib/hexlet_code/tag.rb'))
+  autoload(:HexletCode, File.expand_path('./lib/hexlet_code/form_builder.rb'))
   def self.form_for(item, **args)
     @entity = item
     @result = []
