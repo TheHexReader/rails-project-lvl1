47534ed444fa56b732bda1efff7d07dbc9a35d9b
Hex <thehexreader@gmail.com>
2022-08-15 12:16:03 +0300

    fixing

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,49 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
-require_relative 'hexlet_code/tag'
 
 # HexletCode module
 module HexletCode
+  autoload(:Tag, './lib/hexlet_code/tag.rb')
+  autoload(:HexletCode, './lib/hexlet_code/form_builder.rb')
   def self.form_for(item, **args)
     @entity = item
     @result = []
 
-    args_for_form_build = { **args.except(:url), action: args[:url].nil? ? '#' : args[:url], method: 'post' }
-    @result << HexletCode::Tag.build('form', **args_for_form_build)[0..-8]
-
     yield(HexletCode)
 
-    @result << '</form>'
-    @result.join("\n")
-  end
-
-  def self.input(name, **args)
-    callback(name)
-    @result << "  #{HexletCode::Tag.build('label', for: name) { name.to_s.capitalize }}"
-    if args[:as].nil?
-      process_input_as_input(name)
-    elsif args[:as] == :text
-      process_input_as_text(name)
-    end
-  end
-
-  def self.submit(*value)
-    args_for_submit_build = { name: 'commit', type: 'submit', value: value[0].nil? ? 'Save' : value[0] }
-    @result << "  #{HexletCode::Tag.build('input', **args_for_submit_build)}"
-  end
-
-  def self.callback(name)
-    @entity.public_send(name)
-  end
-
-  def self.process_input_as_input(name)
-    args_for_input_build = { class: 'user-input', name: name, type: 'text' }
-    args_for_input_build[:value] = @entity[name] unless @entity[name].nil?
-    @result << "  #{HexletCode::Tag.build('input', **args_for_input_build)}"
-  end
-
-  def self.process_input_as_text(name)
-    @result << "  #{HexletCode::Tag.build('textarea', cols: '50', rows: '50', name: name) { @entity[name] }}"
+    build_form(@result, args)
   end
 end
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,13 +4,19 @@ module HexletCode
   # Tag building
   module Tag
     def self.build(tag, **attrs)
-      singleton_tags = %w[br img input]
-      result = "<#{tag}#{attrs.empty? ? '' : " #{(attrs.map { |key, item| "#{key}=\"#{item}\"" }).join(' ')}"}>"
+      single_tags = %w[br img input]
+      result = "<#{tag}#{attrs_build(attrs)}>"
 
-      return result if singleton_tags.include? tag
+      return result if single_tags.include? tag
 
       inner_text = yield if block_given?
       "#{result}#{inner_text}</#{tag}>"
+    end
+
+    def self.attrs_build(attrs)
+      return '' if attrs.empty?
+
+      " #{(attrs.map { |key, item| "#{key}=\"#{item}\"" }).join(' ')}"
     end
   end
 end
