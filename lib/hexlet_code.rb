ab05e7adddf25b185b1b6cd51e191beeed4e85f8
Hex <thehexreader@gmail.com>
2022-08-15 12:21:47 +0300

    fixing

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
+require_relative 'hexlet_code/tag'
+require_relative 'hexlet_code/form_builder'
 
 # HexletCode module
 module HexletCode
